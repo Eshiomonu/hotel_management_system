a09b13392cbd7a50e42357d7efe929d23694a1de
kadirisunday <kadirisunday212@gmail.com>
2025-03-21 12:41:45 +0100

    Added admin dashboard template and implemented the logout features

--- routes/web.php
+++ routes/web.php
@@ -25,4 +25,5 @@ require __DIR__ . '/auth.php';
 // Admin group Middleware
 Route::middleware(['auth', 'roles:admin'])->group(function () {
     Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
+    Route::get('/admin/logout', [AdminController::class, 'AdminLogout'])->name('admin.logout');
 });// End Admin middleware
