25ab8a51dfb87881ad46e9f8860d42f5ba2b07d5
kadirisunday <kadirisunday212@gmail.com>
2025-03-21 18:37:14 +0100

    admin dashboard setup

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 
 class AdminController extends Controller
 {
@@ -23,6 +24,46 @@ class AdminController extends Controller
 
         $request->session()->regenerateToken();
 
-        return redirect('/');
+        return redirect('/admin/login');
     }
+
+    public function AdminLogin()
+    {
+
+
+        return view('admin.admin_login');
+    }
+    public function AdminProfile()
+    {
+
+        $id = Auth::user()->id;
+        $profileData = User::find($id);
+        return view('admin.admin_profile_view', compact('profileData'));
+    } // End Method 
+
+    public function AdminProfileStore(Request $request)
+    {
+
+        $id = Auth::user()->id;
+        $data = User::find($id);
+        $data->name = $request->name;
+        $data->email = $request->email;
+        $data->phone = $request->phone;
+        $data->address = $request->address;
+
+        if ($request->file('photo')) {
+            $file = $request->file('photo');
+            @unlink(public_path('upload/admin_images/' . $data->photo));
+            $filename = date('YmdHi') . $file->getClientOriginalName();
+            $file->move(public_path('upload/admin_images'), $filename);
+            $data['photo'] = $filename;
+        }
+        $data->save();
+        $notification = array(
+            'message' => 'Admin Profile Updated Successfully',
+            'alert-type' => 'success'
+        );
+
+        return redirect()->back()->with($notification);
+    } // End Method 
 }
--- routes/web.php
+++ routes/web.php
@@ -26,4 +26,9 @@ require __DIR__ . '/auth.php';
 Route::middleware(['auth', 'roles:admin'])->group(function () {
     Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
     Route::get('/admin/logout', [AdminController::class, 'AdminLogout'])->name('admin.logout');
-});// End Admin middleware
+    Route::get('/admin/profile', [AdminController::class, 'AdminProfile'])->name('admin.profile');
+    Route::post('/admin/profile/store', [AdminController::class, 'AdminProfileStore'])->name('admin.profile.store');
+}); // End Admin middleware
+Route::get('/admin/login', [AdminController::class, 'AdminLogin'])->name('admin.login');
+
+// Route::get('/admin/profile', [AdminController::class, 'AdminProfile'])->name('admin.profile');
