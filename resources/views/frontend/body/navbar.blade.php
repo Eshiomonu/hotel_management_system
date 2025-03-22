09105898f985cbbff301797a214a93c06101d21f
kadirisunday <kadirisunday212@gmail.com>
2025-03-22 09:24:31 +0100

    Frontend design done

---
+++ resources/views/frontend/body/navbar.blade.php
@@ -0,0 +1,88 @@
+<div class="navbar-area">
+  <!-- Menu For Mobile Device -->
+  <div class="mobile-nav">
+    <a href="index.html" class="logo">
+      <img src="{{asset('frontend/assets/img/logos/logo-1.png')}}" class="logo-one" alt="Logo">
+      <img src="{{asset('frontend/assets/img/logos/footer-logo1.png')}}" class="logo-two" alt="Logo">
+    </a>
+  </div>
+
+  <!-- Menu For Desktop Device -->
+  <div class="main-nav">
+    <div class="container">
+      <nav class="navbar navbar-expand-md navbar-light ">
+        <a class="navbar-brand" href="index.html">
+          <img src="{{asset('frontend/assets/img/logos/logo-1.png')}}" class="logo-one" alt="Logo">
+          <img src="{{asset('frontend/assets/img/logos/footer-logo1.png')}}" class="logo-two" alt="Logo">
+        </a>
+
+        <div class="collapse navbar-collapse mean-menu" id="navbarSupportedContent">
+          <ul class="navbar-nav m-auto">
+            <li class="nav-item">
+              <a href="#" class="nav-link active">
+                Home
+
+              </a>
+
+            </li>
+            <li class="nav-item">
+              <a href="about.html" class="nav-link">
+                About
+              </a>
+            </li>
+            <li class="nav-item">
+              <a href="restaurant.html" class="nav-link">
+                Restaurant & Lounge
+              </a>
+            </li>
+
+
+
+
+
+
+
+
+
+            <li class="nav-item">
+              <a href="#" class="nav-link">
+                Services
+
+              </a>
+
+            </li>
+
+            <li class="nav-item">
+              <a href="#" class="nav-link">
+                Blog
+
+              </a>
+            </li>
+
+            <li class="nav-item">
+              <a href="#" class="nav-link">
+                Rooms
+
+              </a>
+
+            </li>
+
+            <li class="nav-item">
+              <a href="contact.html" class="nav-link">
+                Contact
+              </a>
+            </li>
+
+            <li class="nav-item-btn">
+              <a href="#" class="default-btn btn-bg-one border-radius-5">Book Now</a>
+            </li>
+          </ul>
+
+          <div class="nav-btn">
+            <a href="#" class="default-btn btn-bg-one border-radius-5">Book Now</a>
+          </div>
+        </div>
+      </nav>
+    </div>
+  </div>
+</div>
---
+++ resources/views/frontend/home/room_area_two.blade.php
@@ -0,0 +1,24 @@
+<div class="book-area-two pt-100 pb-70">
+  <div class="container">
+    <div class="row align-items-center">
+      <div class="col-lg-6">
+        <div class="book-content-two">
+          <div class="section-title">
+            <span class="sp-color">MAKE A QUICK BOOKING</span>
+            <h2>We Are the Best in All-time & So Please Get a Quick Booking</h2>
+            <p>
+              Experience convenience, security, and the best deals when you book with us. Start planning your next trip now and make unforgettable memories.
+            </p>
+          </div>
+          <a href="#" class="default-btn btn-bg-three">Book Now</a>
+        </div>
+      </div>
+
+      <div class="col-lg-6">
+        <div class="book-img-2">
+          <img src="{{asset('frontend/assets/img/room/deluxe5.jpg')}}" alt="Images">
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
